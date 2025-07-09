8fd6ae0eaa101125af5567f0b7b82f7fc0ca82dc
soham <soaham1006@gmail.com>
2025-07-09 20:33:37 +0530

    Updated MongoDB URI to Atlas

--- client/src/ChatRoom.js
+++ client/src/ChatRoom.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 import { decrypt, encrypt } from "./utils/cryptoUtils";
 
-const socket = io("http://localhost:5000"); // Replace if deployed
+const socket = io("vaulttalk.netlify.app"); 
 
 function ChatRoom({ username, roomId }) {
   const [input, setInput] = useState("");
